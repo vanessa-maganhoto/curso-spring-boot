ad07fa27813da30bd9aafacb19eef48b344846d3
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-05-02 16:31:55 -0300

    Criação do projeto

---
+++ src/main/java/resources/UserResource.java
@@ -0,0 +1,12 @@
+package resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+}
+
+
